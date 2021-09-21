8cc152a8a0f8c76944a1038f663c92cec4320340
WuhaoCHN <90092040+WuhaoCHN@users.noreply.github.com>
2021-09-21 11:12:15 +0800

    Update test_SIMD_mannul.cpp

--- DirectCNN_SIMD/test_SIMD_mannul.cpp
+++ DirectCNN_SIMD/test_SIMD_mannul.cpp
@@ -25,10 +25,13 @@ int main()
 	int pixels = width * height * channel ;  
 	int piece = width * height;
 	cout << "Please enter the input data: " << endl;
-	float** img = new float* [batchsize];
+	//float** img = new float* [batchsize];
+
+	float** img = (float**)_aligned_malloc(sizeof(float*) * batchsize, 16);
 	for (int i = 0; i < batchsize; ++i)
 	{
-		img[i] = new float[pixels];
+//		img[i] = new float[pixels];
+		img[i] = (float*)_aligned_malloc(pixels * sizeof(float), 16);
 		for (int j = 0; j < channel; ++j)
 			for(int k = 0 ; k < height ; ++k)
 				for (int l = 0; l < width; ++l)
@@ -36,7 +39,7 @@ int main()
 					if (l < pad || width - l - 1 < pad || k < pad || height - k - 1 < pad)
 						img[i][j * height * width + k * width + l] = 0;
 					else
-						cin >> img[i][j * height * width + k * width + l];
+						cin >>img[i][j * height * width + k * width + l];
 					//	cin >> img[i][j * height * width + k * width + l];
 				}
 	} 
@@ -49,10 +52,12 @@ int main()
 		channel_re = channel;
 	// img_re[i]: width * height * channel_re;
 	int pixels_re = width * height * channel_re;
-	float** img_re = new float* [batchsize];
+//	float** img_re = new  float* [batchsize];
+	float** img_re = (float**)_aligned_malloc(sizeof(float*) * batchsize,16);
 	for (int i = 0; i < batchsize; ++i)
 	{
-		img_re[i] = new float[pixels_re];
+//		img_re[i] = new float[pixels_re];
+		img_re[i] = (float*)_aligned_malloc(pixels_re * sizeof(float), 16);
 		for (int j = 0; j < height; ++j)
 			for (int k = 0; k < width; ++k)
 				for (int l = 0; l < channel_re; ++l)
@@ -64,17 +69,16 @@ int main()
 				}
 	}
 	for (int i = 0; i < batchsize; ++i)
-		delete[]img[i];
-	delete[]img;
+		_aligned_free(img[i]);
+	_aligned_free(img);
 
 	int pix_per_channel_re = channel_re / 4;
-	__m128** img_mm = new __m128 * [batchsize];
+//	__m128** img_mm = new __m128 * [batchsize];
+	__m128** img_mm = (__m128**)_aligned_malloc(sizeof(__m128*) * batchsize,16);
 	for (int i = 0; i < batchsize; ++i)
 	{
-//		img_mm[i] = new __declspec(align(128)) __m128[width * height];
-		img_mm[i] = new __declspec(align(16)) __m128[width * height];
-
-
+//		img_mm[i] = new __declspec(align(16)) __m128[width * height];
+		img_mm[i] = (__m128*)_aligned_malloc(sizeof(__m128)*(width*height),16);
 		for (int j = 0; j < width * height; ++j) //channel_re
 			for (int k = 0; k < pix_per_channel_re; ++k)
 			{
@@ -82,8 +86,8 @@ int main()
 			}
 	} 
 	for (int i = 0; i < batchsize; ++i)
-		delete[]img_re[i];
-	delete[]img_re;
+		_aligned_free(img_re[i]);
+	_aligned_free(img_re);
 
 	//kernel
 	int ksize, kchannel, kchannel_re, knumber, stride;
@@ -103,11 +107,13 @@ int main()
 	cin >> pnumber;
 	cout << "Please enter the weights of convolutuonal kernel: " << endl;
 	int weights = ksize * ksize * kchannel * knumber;
-	float* kernel = new __declspec(align(16)) float[weights];
+//	float* kernel = new __declspec(align(16)) float[weights];
+	float* kernel = (float*)_aligned_malloc(sizeof(float) * weights,16);
 	for (int i = 0; i < weights; ++i)
 		//	cin >> kernel[i];
 		cin >> kernel[i];
-	float* kernel_re = new __declspec(align(16)) float[ksize * ksize * kchannel_re * knumber];
+//	float* kernel_re = new __declspec(align(16)) float[ksize * ksize * kchannel_re * knumber];
+	float* kernel_re = (float*)_aligned_malloc(sizeof(float) * (ksize * ksize * kchannel_re * knumber), 16);
 	for(int i = 0 ; i < knumber; ++i)
 		for(int j = 0; j < ksize; ++j)
 			for(int k = 0; k < ksize; ++k)
@@ -118,19 +124,25 @@ int main()
 					else
 						kernel_re[i * kchannel_re * ksize * ksize + j * kchannel_re * ksize + k * kchannel_re + l] = 0;
 				}
-	delete[] kernel;
-	__m128* kernel_mm = new __declspec(align(16)) __m128[ksize * ksize * knumber];
+//	delete[] kernel;
+	_aligned_free(kernel);
+
+//	__m128* kernel_mm = new __declspec(align(16)) __m128[ksize * ksize * knumber];
+	__m128* kernel_mm = (__m128*)_aligned_malloc(sizeof(__m128) * (ksize * ksize * knumber),16);
+
 	for (int i = 0; i < ksize * ksize * knumber; ++i)
 		kernel_mm[i] = _mm_load_ps(&kernel_re[4 * i]);
-	delete[] kernel_re;
+	_aligned_free(kernel_re);
 
 	int ochannel = knumber;
 	int owidth = (width - ksize) / stride + 1;
 	int oheight = (height - ksize) / stride + 1;
-	float** output = new float* [batchsize];
+//	float** output = new float* [batchsize]
+	float** output = (float**)_aligned_malloc(sizeof(float*) * batchsize,16);
 	for (int i = 0; i < batchsize; ++i)
 	{
-		output[i] = new __declspec(align(16)) float[owidth * oheight * ochannel];
+	//	output[i] = new __declspec(align(16)) float[owidth * oheight * ochannel];
+		output[i] = (float*)_aligned_malloc(sizeof(float) * (owidth * oheight * ochannel),16);
 		for (int j = 0; j < owidth * oheight * ochannel; ++j)
 			output[i][j] = bias;
 	}
@@ -165,10 +177,12 @@ int main()
 	else
 		ochannel_re = ochannel;
 	int opixel_re = owidth * oheight * ochannel_re;
-	float** output_re = new float* [batchsize];
+//	float** output_re = new float* [batchsize];
+	float** output_re = (float**)_aligned_malloc(sizeof(float*) * batchsize,16);
 	for (int i = 0; i < batchsize; ++i)
 	{
-		output_re[i] = new __declspec(align(16)) float[opixel_re];
+//		output_re[i] = new __declspec(align(16)) float[opixel_re];
+		output_re[i] = (float*)_aligned_malloc(sizeof(float) * opixel_re,16);
 		for(int j = 0; j < oheight; ++j)
 			for(int k = 0; k < owidth; ++k)
 				for (int l = 0; l < ochannel_re; ++l)
@@ -182,10 +196,12 @@ int main()
 	}
 	int pix_per_ochannel_re = ochannel_re / 4;
 
-	__m128** output_mm = new __m128 * [batchsize];
+//	__m128** output_mm = new __m128 * [batchsize];
+	__m128** output_mm = (__m128**)_aligned_malloc(sizeof(__m128*) * batchsize,16);
 	for (int i = 0; i < batchsize; ++i)
 	{
-		output_mm[i] = new __declspec(align(16)) __m128[owidth * oheight*pix_per_ochannel_re];
+//		output_mm[i] = new __declspec(align(16)) __m128[owidth * oheight*pix_per_ochannel_re];
+		output_mm[i] = (__m128*)_aligned_malloc(sizeof(__m128) * (owidth * oheight * pix_per_ochannel_re),16);
 		for (int j = 0; j < oheight * owidth; ++j)
 			for (int k = 0; k < pix_per_ochannel_re; ++k)
 			{
@@ -200,10 +216,13 @@ int main()
 	
 	int pwidth = owidth / pnumber;
 	int pheight = oheight / pnumber;
-	__m128** pout = new __m128 * [batchsize];
+//	__m128** pout = new __m128 * [batchsize];
+	__m128** pout = (__m128**)_aligned_malloc(sizeof(__m128*) * batchsize,16);
+
 	for (int i = 0; i < batchsize; ++i)
 	{
-		pout[i] = new __declspec(align(16)) __m128[pwidth * pheight * pix_per_ochannel_re];
+//		pout[i] = new __declspec(align(16)) __m128[pwidth * pheight * pix_per_ochannel_re];
+		pout[i] = (__m128*)_aligned_malloc(sizeof(__m128) * (pwidth * pheight * pix_per_ochannel_re),16);
 		for (int j = 0; j < pwidth * pheight * pix_per_ochannel_re; ++j)
 		{
 			float __declspec(align(16)) am[4] = { 0,0,0,0 };
@@ -218,10 +237,13 @@ int main()
 	cout << "Time consumed by Pooling layer using SIMD: " << endTime - startTime << endl;
 
 	cout << "The output of pooling layer: " << endl;
-	float** p_out = new float* [batchsize *pheight *pwidth* pix_per_ochannel_re];
+//	float** p_out = new float* [batchsize *pheight *pwidth* pix_per_ochannel_re];
+	float** p_out = (float**)_aligned_malloc(sizeof(float*) * (batchsize * pheight * pwidth * pix_per_ochannel_re),16);
+
 	for (int i = 0; i < batchsize * pheight * pwidth * pix_per_ochannel_re; ++i)
 	{
-		p_out[i] = new __declspec(align(16)) float[4];
+	//	p_out[i] = new __declspec(align(16)) float[4];
+		p_out[i] = (float*)_aligned_malloc(sizeof(float) * 4 ,16);
 		_mm_store_ps(p_out[i], pout[i / (pwidth * pheight * pix_per_ochannel_re)][i % (pwidth * pheight * pix_per_ochannel_re)]);
 	}
 	for (int i = 0; i < batchsize; ++i)
@@ -242,13 +264,16 @@ int main()
 	}
 
 	for (int i = 0; i < batchsize * pheight * pwidth * pix_per_ochannel_re; ++i)
-		delete[]p_out[i];
-	delete[]p_out;
+		_aligned_free(p_out[i]);
+	_aligned_free(p_out);
 
-	float** pooling_out = new float* [batchsize];
+//	float** pooling_out = new float* [batchsize];
+	float** pooling_out = (float**)_aligned_malloc(sizeof(float*) * batchsize,16);
 	for (int i = 0; i < batchsize; ++i)
 	{
-		pooling_out[i] = new __declspec(align(16)) float[pwidth * pheight * knumber];
+//		pooling_out[i] = new __declspec(align(16)) float[pwidth * pheight * knumber];
+		pooling_out[i] = (float*)_aligned_malloc(sizeof(float) * (pwidth * pheight * knumber),16);
+
 		for (int j = 0; j < pwidth * pheight * knumber; ++j)
 			pooling_out[i][j] = 0;
 	}
@@ -266,21 +291,24 @@ int main()
 
 
 	for (int i = 0; i < batchsize; ++i)
-		delete[]output_re[i];
-	delete[]output_re;
+		_aligned_free(pout[i]);
+	_aligned_free(pout);
 	for (int i = 0; i < batchsize; ++i)
-		delete[]img_mm[i];
-	delete[]img_mm;
+		_aligned_free(output_re[i]);
+	_aligned_free(output_re);
 	for (int i = 0; i < batchsize; ++i)
-		delete[]pooling_out[i];
-	delete[]pooling_out;
+		_aligned_free(img_mm[i]);
+	_aligned_free(img_mm);
 	for (int i = 0; i < batchsize; ++i)
-		delete[]output_mm[i];
-	delete[]output_mm;                                           
+		_aligned_free(pooling_out[i]);
+	_aligned_free(pooling_out);
 	for (int i = 0; i < batchsize; ++i)
-		delete[]output[i];
-	delete[]output;
-	delete[] kernel_mm;
+		_aligned_free(output_mm[i]);
+	_aligned_free(output_mm);
+	for (int i = 0; i < batchsize; ++i)
+		_aligned_free(output[i]);
+	_aligned_free(output);
+	_aligned_free(kernel_mm);
 	system("pause");    
 	return 0;
 }
